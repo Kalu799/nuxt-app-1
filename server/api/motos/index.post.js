db1a91a589c2d0d73143bdaf9229645fcaab9a8a
Lukas <lukasfalisse@gmail.com>
2026-08-27 14:41:30 +0200

    edit post motos

--- server/api/motos/index.post.js
+++ server/api/motos/index.post.js
@@ -1,13 +1,6 @@
 export default defineEventHandler(async (e) => {
   const body = await readBody(e)
 
-  if(!body || !body.marque || !body.model || !body.couleur) {
-    throw createError({
-      statusCode: 400,
-      statusMessage: `Les champs title et duration sont requis.`
-    })
-  }
-
   try {
     const [result] = await db.query(
       'INSERT INTO motos (marque, modele, couleur) VALUES (?, ?, ?)',
