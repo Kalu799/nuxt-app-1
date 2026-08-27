5e75f30cb51928c328bbb40c24f004f3246e0dde
Lukas <lukasfalisse@gmail.com>
2026-08-27 14:05:38 +0200

    edit host

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   runtimeConfig: {
-    dhHost: 'srv660.hstgr.io',
+    dhHost: '77.37.35.163',
     dbPort: 3306,
     dbUser: 'u868520261_nuxt',
     dbName: 'u868520261_nuxt',
